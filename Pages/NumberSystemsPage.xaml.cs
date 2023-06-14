acbb38aa7af4af1f1c83989986f99ec2ff91d40b
eue <49861580+KDVHELL@users.noreply.github.com>
2023-06-14 20:59:42 +0500

    numsys logic

--- Pages/NumberSystemsPage.xaml.cs
+++ Pages/NumberSystemsPage.xaml.cs
@@ -43,7 +43,39 @@ namespace CalcYouLate.Pages
 
         private void NumberSystems_Result()
         {
-            
+            int fromSS = int.Parse(inputNumsys.Text);
+            int toSS = int.Parse(outputNumsys.Text);
+            string res = FromTenToSS(toSS, FromSSToTen(fromSS));
+            outputNum.Text = res;
+        }
+
+        private int FromSSToTen(int fromSS)
+        {
+            int res = 0;
+            string alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string input = inputNum.Text;
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                string symbol = c.ToString();
+                if (alphabet.IndexOf(symbol) != -1)
+                {
+                    res += alphabet.IndexOf(symbol)*(int)Math.Pow(fromSS, 36 - i);
+                }
+            }
+            return res;
+        }
+
+        private string FromTenToSS(int toSS, int input)
+        {
+            int inputNum = input;
+            string res = "";
+            while (inputNum > 0)
+            {
+                res += (inputNum % toSS).ToString();
+                inputNum = inputNum / toSS;
+            }
+            return res;
         }
 
         private void outputResult_Click(object sender, RoutedEventArgs e)
